cc5af1a11b0415e157999456ad6045c6a2b1de0d
trabalhosuft <leonardorec1@gmail.com>
2019-05-30 16:21:09 -0300

    adds herinson's code to load files
    if i got it right, he used ajax and github api

---
+++ layout.js
@@ -0,0 +1,28 @@
+const main = document.querySelector(".pdfs");
+
+// troca pelo teu nome de usuário, repositório e a pasta onde os PDFs estão
+const owner = "leonardorez"
+const repo = "scratch-tuto"
+const path = ""
+
+fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`)
+  .then(response => response.json()).then(response => {
+    response.map(item => {
+      var loadingTask = pdfjsLib.getDocument(item.download_url);
+      loadingTask.promise.then((pdf) => {
+        const main = document.querySelector("main");
+        const div = document.querySelector("#pdf-item");
+        const cln = div.cloneNode(true);
+
+        cln.style.display = "block";
+        const link = cln.querySelector("a");
+        const span = cln.querySelector("span");
+
+        link.onclick = () => window.open(`https://mozilla.github.io/pdf.js/web/viewer.html?file=${item.download_url}`);
+        link.title = `Open this PDF in a new tab`;
+        span.textContent = item.name;
+        main.querySelector("h2").textContent = "";
+        main.appendChild(cln);
+      });
+    })
+  });
